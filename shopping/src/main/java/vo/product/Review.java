0fe67d107ea0a7eec72828ca32e18134a30a92e8
ddingjin <106426770+uengae@users.noreply.github.com>
2023-05-31 15:16:06 +0900

    vo.product 내용 수정 및 util DBUtil.java 생성

--- shopping/src/main/java/vo/product/Review.java
+++ shopping/src/main/java/vo/product/Review.java
@@ -1,5 +1,47 @@
 package vo.product;
 
 public class Review {
-
+	private int reviewNo;
+	private int oderNo;
+	private String reviewTitle;
+	private String reviewContent;
+	private String createdate ;
+	private String updatedate ;
+	public int getReviewNo() {
+		return reviewNo;
+	}
+	public void setReviewNo(int reviewNo) {
+		this.reviewNo = reviewNo;
+	}
+	public int getOderNo() {
+		return oderNo;
+	}
+	public void setOderNo(int oderNo) {
+		this.oderNo = oderNo;
+	}
+	public String getReviewTitle() {
+		return reviewTitle;
+	}
+	public void setReviewTitle(String reviewTitle) {
+		this.reviewTitle = reviewTitle;
+	}
+	public String getReviewContent() {
+		return reviewContent;
+	}
+	public void setReviewContent(String reviewContent) {
+		this.reviewContent = reviewContent;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	
 }
