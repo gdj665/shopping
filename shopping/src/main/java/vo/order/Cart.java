d64cf9e24a778a1a165d117b43d17ed82fbae45e
DWJ <whehddnr999@naver.com>
2023-05-31 15:04:37 +0900

    vo.order/Cart.java, vo.order/Orders.java, vo.cs/Qa.java 3파일 클래스에 변수 선언 및 캡슐화 진행

--- shopping/src/main/java/vo/order/Cart.java
+++ shopping/src/main/java/vo/order/Cart.java
@@ -1,5 +1,56 @@
 package vo.order;
 
 public class Cart {
-
+	private int cartNo;
+	private String id;
+	private int product_no;
+	private int cartCnt;
+	private String productSaveFilename;
+	private String createdate;
+	private String updatedate;
+	
+	public String getProductSaveFilename() {
+		return productSaveFilename;
+	}
+	public void setProductSaveFilename(String productSaveFilename) {
+		this.productSaveFilename = productSaveFilename;
+	}
+	public int getCartNo() {
+		return cartNo;
+	}
+	public void setCartNo(int cartNo) {
+		this.cartNo = cartNo;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getProduct_no() {
+		return product_no;
+	}
+	public void setProduct_no(int product_no) {
+		this.product_no = product_no;
+	}
+	public int getCartCnt() {
+		return cartCnt;
+	}
+	public void setCartCnt(int cartCnt) {
+		this.cartCnt = cartCnt;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	
+	
 }
--- shopping/src/main/java/vo/order/Orders.java
+++ shopping/src/main/java/vo/order/Orders.java
@@ -1,5 +1,63 @@
 package vo.order;
 
 public class Orders {
-
+	private int orderNo;
+	private String id;
+	private int orderStatus;
+	private int orderPrice;
+	private int orderPointUse;
+	private String productSaveFilename;
+	private String createdate;
+	private String updatedate;
+	
+	public String getProductSaveFilename() {
+		return productSaveFilename;
+	}
+	public void setProductSaveFilename(String productSaveFilename) {
+		this.productSaveFilename = productSaveFilename;
+	}
+	public int getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(int orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	public int getOrderPrice() {
+		return orderPrice;
+	}
+	public void setOrderPrice(int orderPrice) {
+		this.orderPrice = orderPrice;
+	}
+	public int getOrderPointUse() {
+		return orderPointUse;
+	}
+	public void setOrderPointUse(int orderPointUse) {
+		this.orderPointUse = orderPointUse;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	
+	
 }
