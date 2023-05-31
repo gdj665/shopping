615f6b6e97de8d133b8cf32733454bab54ca0101
ddingjin <106426770+uengae@users.noreply.github.com>
2023-05-31 16:16:36 +0900

    test5

--- shopping/src/main/java/dao/main/MainDao.java
+++ shopping/src/main/java/dao/main/MainDao.java
@@ -1,5 +1,12 @@
 package dao.main;
 
-public class MainDao {
+import util.DBUtil;
+import vo.product.Product;
 
+public class MainDao {
+	public Product selectRecentlyProduct() {
+		DBUtil conn = new DBUtil();
+		String sql = "SELECT FROM product ORDER BY "
+		return null;
+	}
 }
