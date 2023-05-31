75b1daf15e24c982228bed503195a3ba00e4752c
JaesungLee01 <vertual070@naver.com>
2023-05-31 16:17:58 +0900

    vo id employees

--- shopping/src/main/java/vo/id/Employees.java
+++ shopping/src/main/java/vo/id/Employees.java
@@ -1,5 +1,39 @@
 package vo.id;
 
 public class Employees {
-
+	private String id;
+	private String empName;
+	private int empLevel;
+	private String createdate;
+	private String updatedate;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public int getEmpLevel() {
+		return empLevel;
+	}
+	public void setEmpLevel(int empLevel) {
+		this.empLevel = empLevel;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
 }
